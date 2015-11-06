deeca34c260a029a7af0b511eaedece19011415b
Thomas Weber <thweber@mail.hs-ulm.de>
2015-11-06 14:10:13 +0100

    tested http request

---
+++ php/party_1.php
@@ -0,0 +1,46 @@
+<?php
+
+	echo "app is starting...\n";
+
+	header("Content-Type:application/json");
+	//$http = new HTTPResponse();
+// ******* empfangen von http request *******
+
+	if(!empty($_GET['data'])){
+		$data=$_GET['data'];
+	
+		if (empty($_GET['data'])){
+			deliver_response(400, "error", NULL);
+		}
+		else{
+			deliver_response(200, "all right", $data);
+		}
+	}
+	
+// ******* antwort als json formatieren ********
+	function deliver_response($status, $status_message, $msg)
+	{
+		header("HTTP/1.1 $status $status_message");
+		
+		$response['status']=$status;
+		$response['status_message']=$status_message;
+		$response['msg']=$msg;
+		
+		$json_response=json_encode($response);
+		
+		echo $json_response;
+	}
+
+
+
+// ****** Zugriff auf Datenbank ******
+$mysqli = new mysqli("localhost", "root", "", "gluecksrad_party_1");
+
+if($mysqli-> connect_error){
+    echo "not able to connect to database" . mysqli_connect_error();
+    exit();
+}
+
+echo "\nconnected";
+
+?>
